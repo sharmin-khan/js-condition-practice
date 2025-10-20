a6aeef8586060d8b15ba01ee5d470c7ff30b388e
sharmin-khan <sharminkhancse@gmail.com>
2025-10-20 08:57:46 +0600

    Check whether a given number is positive, negative, or zero.

--- problem.js
+++ problem.js
@@ -1,8 +1,20 @@
 // 1. Write a program to check whether a number is even or odd.
-let number = 7;
-if (number % 2 === 0){
-    console.log("Even Number")
+// let number = 7;
+// if (number % 2 === 0){
+//     console.log("Even Number")
+// }
+// else{
+//     console.log("Odd Number")
+// }
+
+// 2. Check whether a given number is positive, negative, or zero.
+let number = -3;
+if(number > 0){
+    console.log("Number is positive")
+}
+else if (number < 0){
+    console.log("Number is Negative")
 }
 else{
-    console.log("Odd Number")
+    console.log("Zero")
 }
